cf3ba689a20c874cf1f35873b59aeff327b2049e
fiodar.sazanavets@csl-group.com <fiodar.sazanavets@csl-group.com>
2021-07-17 14:56:30 +0100

    Add code samples for parts 1.1-1.2 of Chapter 1

---
+++ Chapter-01/Parts 1.1-1.2/BasicGrpcService/BasicGrpcService/Services/GreeterService.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Grpc.Core;
+
+namespace BasicGrpcService
+{
+    public class GreeterService : Greeter.GreeterBase
+    {
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new HelloReply
+            {
+                Message = "Hello " + request.Name
+            });
+        }
+    }
+}
