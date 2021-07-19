dfd19c734b3bbd6a74b0d948c844fcade2e48d94
fiodar.sazanavets@csl-group.com <fiodar.sazanavets@csl-group.com>
2021-07-19 08:29:54 +0100

    Add remaining code samples for chapter 1

---
+++ Chapter-01/Part 1.3/BasicGrpcService/BasicGrpcClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Greet;
+using Grpc.Net.Client;
+
+namespace BasicGrpcClient
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            // The port number(5001) must match the port of the gRPC server.
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(
+                              new HelloRequest { Name = "BasicGrpcClient" });
+            Console.WriteLine("Greeting: " + reply.Message);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Chapter-01/Part 1.4/BasicGrpcService/GrpcDependencies/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace GrpcDependencies
+{
+    public class Class1
+    {
+    }
+}
