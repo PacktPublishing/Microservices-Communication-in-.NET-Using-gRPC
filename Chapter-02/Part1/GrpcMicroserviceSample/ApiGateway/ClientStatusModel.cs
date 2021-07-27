9b090aee7f75d15f6e4340e7c40b9a3c06ef7d5c
fiodar.sazanavets@csl-group.com <fiodar.sazanavets@csl-group.com>
2021-07-27 16:19:37 +0100

    Add code samples for part 1 of chapter 2

---
+++ Chapter-02/Part1/GrpcMicroserviceSample/ApiGateway/ClientStatusModel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace ApiGateway
+{
+    public class ClientStatusModel
+    {
+        public string Name { get; set; }
+        public ClientStatus Status { get; set; }
+    }
+}
