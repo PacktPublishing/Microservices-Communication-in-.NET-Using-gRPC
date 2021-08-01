0f47d8cc0974db52636929296314c2fa2d89d067
fiodar.sazanavets@csl-group.com <fiodar.sazanavets@csl-group.com>
2021-08-01 14:36:59 +0100

    Add code samples for part 4 of chapter 2.

---
+++ Chapter-02/Part4/SignalrApplication/Hubs/JobsHub.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalrApplication.Hubs
+{
+    public class JobsHub : Hub
+    {
+        public async Task SendSingleJob(string jobDescription)
+        {
+            await Clients.Caller.SendAsync("ReceiveMessage", $"Job executed successfully. Description: {jobDescription}");
+        }
+
+        public async Task StreamJobs(IAsyncEnumerable<int> stream)
+        {
+            var jobsCount = 0;
+            await foreach (var item in stream)
+            {
+                Console.WriteLine($"Job {item} executed succesfully");
+                jobsCount++;
+            }
+
+            await Clients.Caller.SendAsync("ReceiveMessage", $"{jobsCount} jobs executed successfully.");
+        }
+
+        public async IAsyncEnumerable<string> TriggerJobs(
+        int jobsCount,
+        [EnumeratorCancellation]
+        CancellationToken cancellationToken)
+        {
+            for (var i = 0; i < jobsCount; i++)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+
+                yield return $"Job {i} executed succesfully";
+
+                await Task.Delay(2000, cancellationToken);
+            }
+        }
+    }
+}
---
+++ Chapter-02/Part4/SignalrApplication/wwwroot/js/site.js
@@ -0,0 +1,58 @@
+﻿const connection = new signalR.HubConnectionBuilder()
+    .withUrl("/jobsHub")
+    .configureLogging(signalR.LogLevel.Information)
+    .build();
+
+connection.on("ReceiveMessage", (message) => addMessage(message));
+
+$('#btn-send-single').click(function () {
+    var jobDescription = $('#job-description').val();
+
+    connection.invoke("SendSingleJob", jobDescription).catch(err => console.error(err.toString()));
+});
+
+$('#btn-send-multiple').click(function () {
+    var numberOfJobs = parseInt($('#jobs-to-send').val(), 10);
+    var subject = new signalR.Subject();
+    
+    var iteration = 0;
+    var intervalHandle = setInterval(() => {
+        iteration++;
+        subject.next(iteration);
+        if (iteration === numberOfJobs) {
+            clearInterval(intervalHandle);
+            subject.complete();
+        }
+    }, 2000);
+
+    connection.send("StreamJobs", subject);
+});
+
+$('#btn-trigger-multiple').click(function () {
+    var numberOfJobs = parseInt($('#jobs-to-trigger').val(), 10);
+
+    connection.stream("TriggerJobs", numberOfJobs)
+        .subscribe({
+            next: (message) => addMessage(message)
+        });
+});
+
+async function start() {
+    try {
+        await connection.start();
+        console.log('connected');
+    } catch (err) {
+        console.log(err);
+        setTimeout(() => start(), 5000);
+    }
+};
+
+connection.onclose(async () => {
+    await start();
+});
+
+start();
+
+function addMessage(message) {
+    $('#signalr-message-panel').prepend($('<div />').text(message));
+}
