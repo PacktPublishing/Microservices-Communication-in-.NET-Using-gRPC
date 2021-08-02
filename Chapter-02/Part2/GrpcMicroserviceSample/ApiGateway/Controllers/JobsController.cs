27e63955799c132449ca2d2aec26edd4f86ac19c
fiodar.sazanavets@csl-group.com <fiodar.sazanavets@csl-group.com>
2021-08-02 08:48:05 +0100

    Minor code cleaning

--- Chapter-02/Part2/GrpcMicroserviceSample/ApiGateway/Controllers/JobsController.cs
+++ Chapter-02/Part2/GrpcMicroserviceSample/ApiGateway/Controllers/JobsController.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace ApiGateway.Controllers
 {
--- Chapter-02/Part2/GrpcMicroserviceSample/ApiGateway/Controllers/StatusController.cs
+++ Chapter-02/Part2/GrpcMicroserviceSample/ApiGateway/Controllers/StatusController.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace ApiGateway.Controllers
 {
