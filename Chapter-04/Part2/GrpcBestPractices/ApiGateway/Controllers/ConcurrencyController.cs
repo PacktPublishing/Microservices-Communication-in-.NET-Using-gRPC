8e348c69f782a06d3ec4ef8c409f481f3bc66b89
fiodar.sazanavets@csl-group.com <fiodar.sazanavets@csl-group.com>
2021-08-27 10:59:40 +0100

    Add code samples for part 2

---
+++ Chapter-04/Part2/GrpcBestPractices/ApiGateway/Controllers/ConcurrencyController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Performance;
+
+namespace ApiGateway.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ConcurrencyController : ControllerBase
+    {
+        private readonly string serverUrl;
+
+        public ConcurrencyController(IConfiguration configuration)
+        {
+            serverUrl = configuration["ServerUrl"];
+        }
+
+
+        [HttpGet("single-connection/{count}")]
+        public ResponseModel GetDataFromSingleConnection(int count)
+        {
+            using var channel = GrpcChannel.ForAddress(serverUrl);
+            var stopWatch = Stopwatch.StartNew();
+
+            var response = new ResponseModel();
+
+            var concurrentJobs = new List<Task>();
+            for (var i = 0; i < count; i++)
+            {
+                var client = new Monitor.MonitorClient(channel);
+                concurrentJobs.Add(Task.Run(() =>
+                {
+                    client.GetPerformance(new PerformanceStatusRequest { ClientName = $"clinet {i + 1}" });
+                }));
+            }
+
+            Task.WaitAll(concurrentJobs.ToArray());
+
+            response.RequestProcessingTime = stopWatch.ElapsedMilliseconds;
+
+            return response;
+        }
+
+        [HttpGet("multiple-connections/{count}")]
+        public ResponseModel GetDataFromMultipleConnections(int count)
+        {
+            using var channel = GrpcChannel.ForAddress(serverUrl, new GrpcChannelOptions
+            {
+                HttpHandler = new SocketsHttpHandler
+                {
+                    EnableMultipleHttp2Connections = true,
+                }
+            });
+            var stopWatch = Stopwatch.StartNew();
+
+            var response = new ResponseModel();
+
+            var concurrentJobs = new List<Task>();
+            for (var i = 0; i < count; i++)
+            {
+                concurrentJobs.Add(Task.Run(() =>
+                {
+                    var client = new Monitor.MonitorClient(channel);
+                    client.GetPerformance(new PerformanceStatusRequest { ClientName = $"clinet {i + 1}" });
+                }));
+            }
+
+            Task.WaitAll(concurrentJobs.ToArray());
+
+            response.RequestProcessingTime = stopWatch.ElapsedMilliseconds;
+
+            return response;
+        }
+    }
+}
---
+++ Chapter-04/Part2/GrpcBestPractices/PerformanceService/Services/PerformanceMonitor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Performance;
+
+namespace PerformanceService
+{
+    public class PerformanceMonitor : Monitor.MonitorBase
+    {
+
+
+        public override Task<PerformanceStatusResponse> GetPerformance(PerformanceStatusRequest request, ServerCallContext context)
+        {
+            var randomNumberGenerator = new Random();
+
+            return Task.FromResult(new PerformanceStatusResponse
+            {
+                CpuPercentageUsage = randomNumberGenerator.NextDouble() * 100,
+                MemoryUsage = randomNumberGenerator.NextDouble() * 100,
+                ProcessesRunning = randomNumberGenerator.Next(),
+                ActiveConnections = randomNumberGenerator.Next()
+            });
+        }
+    }
+}
