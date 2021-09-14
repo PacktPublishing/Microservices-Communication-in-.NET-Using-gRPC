900dc46f61fd6102088a11474c08a9573d892a75
fiodar.sazanavets@csl-group.com <fiodar.sazanavets@csl-group.com>
2021-09-14 09:11:16 +0100

    Add code samples for part 1 of chapter 5

---
+++ Chapter-05/Part1/ApiVersioning/GrpcServer/Services/StatusService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Stats;
+
+namespace GrpcServer
+{
+    public class StatusService : Stats.Status.StatusBase
+    {
+        public override Task<StatusResponse> GetStatus(StatusRequest request, ServerCallContext context)
+        {
+            Console.WriteLine($"Client name is {request.ClientName}");
+            Console.WriteLine($"Client description is {request.ClientDescription}");
+            Console.WriteLine($"Is client ready? {request.Ready}");
+            Console.WriteLine($"Is client authorized? {request.Authorized}");
+
+            var randomNumberGenerator = new Random();
+
+            return Task.FromResult(new StatusResponse
+            {
+                ServerName = "TestServer",
+                ServerDescription = "This is a test server that is used for generating status metrics",
+                NumberOfConnections = randomNumberGenerator.Next(),
+                CpuUsage = randomNumberGenerator.NextDouble() * 100,
+                MemoryUsage = randomNumberGenerator.NextDouble() * 100,
+                ErrorsLogged = (ulong)randomNumberGenerator.Next(),
+                CatastrophicFailuresLogged = (uint)randomNumberGenerator.Next(),
+                Active = true
+            });
+        }
+    }
+}
