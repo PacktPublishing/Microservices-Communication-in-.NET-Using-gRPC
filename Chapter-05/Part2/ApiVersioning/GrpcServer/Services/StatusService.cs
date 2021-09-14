0e364ee7b4618159e26ec92d2d9ae4ccf1338239
fiodar.sazanavets@csl-group.com <fiodar.sazanavets@csl-group.com>
2021-09-14 14:07:07 +0100

    Addd code samples for parts 2 and 4.

---
+++ Chapter-05/Part2/ApiVersioning/GrpcServer/Services/StatusService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Stats;
+
+namespace GrpcServer
+{
+    public class StatusService : Stats.Status.StatusBase
+    {
+        public override Task<StatusResponse> GetStatus(StatusRequest request, ServerCallContext context)
+        {
+            Console.WriteLine($"Client name is {request.ClientName}");
+            Console.WriteLine($"Client description is {request.ClientDescription}");
+            Console.WriteLine($"Is client ready? {request.Ready}");
+            Console.WriteLine($"Is client authorized? {request.Authorized}");
+
+            var randomNumberGenerator = new Random();
+
+            return Task.FromResult(new StatusResponse
+            {
+                ServerName = "TestServer",
+                ServerDescription = "This is a test server that is used for generating status metrics",
+                NumberOfConnections = randomNumberGenerator.Next(),
+                CpuUsage = randomNumberGenerator.NextDouble() * 100,
+                MemoryUsage = randomNumberGenerator.NextDouble() * 100,
+                CatastrophicFailuresLogged = (uint)randomNumberGenerator.Next(),
+                Busy = true
+            });
+        }
+    }
+}
---
+++ Chapter-05/Part3/ApiVersioning/GrpcClient/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            Console.WriteLine("Please enter gRPC server address:");
+            var serverUrl = Console.ReadLine();
+
+            var client = new Stats.Status.StatusClient(GrpcChannel.ForAddress(serverUrl));
+
+            Console.WriteLine("Please enter client name:");
+            var clientName = Console.ReadLine();
+
+            Console.WriteLine("Please enter client description:");
+            var clientDescription = Console.ReadLine();
+
+            var response = await client.GetStatusAsync(new Stats.StatusRequest
+            {
+                ClientName = clientName,
+                ClientDescription = clientDescription,
+                Authorized = true
+            });
+
+            Console.WriteLine($"Server name: {response.ServerName}");
+            Console.WriteLine($"Server description: {response.ServerDescription}");
+            Console.WriteLine($"Number of connections: {response.NumberOfConnections}");
+            Console.WriteLine($"CPU usage: {response.CpuUsage}");
+            Console.WriteLine($"Memory usage: {response.MemoryUsage}");
+            Console.WriteLine($"Errors logged: {response.ErrorsLogged}");
+            Console.WriteLine($"Catastrophic failures logged: {response.CatastrophicFailuresLogged}");
+            Console.WriteLine($"Active: {response.Active}");
+
+            Console.ReadKey();
+        }
+    }
+}
