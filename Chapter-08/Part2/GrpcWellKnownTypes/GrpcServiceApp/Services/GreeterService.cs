26887c3496add3c0eef9a7200b6ff2d328d72891
fiodar.sazanavets@csl-group.com <fiodar.sazanavets@csl-group.com>
2021-10-16 10:06:46 +0100

    Add code samples for part 2 of chapter 8.

---
+++ Chapter-08/Part2/GrpcWellKnownTypes/GrpcServiceApp/Services/GreeterService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+
+namespace GrpcServiceApp
+{
+    public class GreeterService : Greeter.GreeterBase
+    {
+        private readonly MessageCounter counter;
+
+        public GreeterService(MessageCounter counter)
+        {
+            this.counter = counter;
+        }
+
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            if (!string.IsNullOrWhiteSpace(request.Name))
+            {
+                var message = "Hello " + request.Name;
+                var timeToDeadline = context.Deadline - DateTime.UtcNow;
+                var messageBytes = Encoding.ASCII.GetBytes(message);
+
+                return Task.FromResult(new HelloReply
+                {
+                    Message = message,
+                    MessageProcessedCount = counter.IncrementCount(),
+                    MessageLengthInBytes = (ulong)messageBytes.Length,
+                    MessageLengthInLetters = message.Length,
+                    MillisecondsToDeadline = timeToDeadline.Milliseconds,
+                    SecondsToDeadline = (float)timeToDeadline.TotalSeconds,
+                    MinutesToDeadline = timeToDeadline.TotalMinutes,
+                    LastNamePresent = request.Name.Split(' ').Length > 1,
+                    MessageBytes = Google.Protobuf.ByteString.CopyFrom(messageBytes)
+                });
+            }
+
+            return Task.FromResult(new HelloReply());
+        }
+
+        public override Task<MessageCount> GetMessageProcessedCount(Empty request, ServerCallContext context)
+        {
+            return Task.FromResult(new MessageCount
+            {
+               Count = counter.GetCurrentCount()
+            });
+        }
+
+        public override Task<Empty> SynchronizeMessageCount(MessageCount request, ServerCallContext context)
+        {
+            counter.UpdateCount(request.Count);
+
+            return Task.FromResult(new Empty());
+        }
+    }
+}
