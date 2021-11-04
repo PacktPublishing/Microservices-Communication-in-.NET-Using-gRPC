07dd234358ccf64a88adf3dbdedf2851f795a631
mobiletechtracker <info@mobiletechtracker.co.uk>
2021-11-04 21:41:10 +0000

    Remove unnecessary using

--- Chapter-09/Part3/GrpcTls/UserInfoManager/Startup.cs
+++ Chapter-09/Part3/GrpcTls/UserInfoManager/Startup.cs
@@ -7,7 +7,6 @@ using Microsoft.Extensions.Hosting;
 using System;
 using System.Net;
 using System.Security.Claims;
-using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using UserInfoManager.Services;
 
