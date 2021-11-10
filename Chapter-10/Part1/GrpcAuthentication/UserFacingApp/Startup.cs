d7d5d4f4c1fa6e109d5cc14bccaef52d8ee93cfe
mobiletechtracker <info@mobiletechtracker.co.uk>
2021-11-10 18:06:23 +0000

    Add code samples for part 1 of chapter 10

---
+++ Chapter-10/Part1/GrpcAuthentication/UserFacingApp/Startup.cs
@@ -0,0 +1,67 @@
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace UserFacingApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            JwtSecurityTokenHandler.DefaultMapInboundClaims = false;
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = "Cookies";
+                options.DefaultChallengeScheme = "oidc";
+            })
+            .AddCookie("Cookies")
+            .AddOpenIdConnect("oidc", options =>
+            {
+                options.Authority = "https://localhost:5001";
+                options.ClientId = "userFacingApp";
+                options.ClientSecret = "userFacingAppSecret";
+                options.ResponseType = "code";
+                options.CallbackPath = "/signin-oidc";
+
+                options.SaveTokens = true;
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints
+                    .MapDefaultControllerRoute()
+                    .RequireAuthorization();
+            });
+        }
+    }
+}
